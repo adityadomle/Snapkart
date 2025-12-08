0e62b51b4c85ecf6f2c93a6c4e91c216c3760513
Aditya Domle <adityadomle14@gmail.com>
2025-12-08 22:46:17 +0530

    Fix formatting in Provider component

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { SessionProvider } from 'next-auth/react'
 import React from 'react'
-
+ 
 function Provider({children}:{children:React.ReactNode}) {
   return (
     <SessionProvider>
